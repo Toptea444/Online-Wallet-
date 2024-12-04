6e71324ad50c776e057edd973a403f9f6fe7cf3b
Toptea444 <142181008+Toptea444@users.noreply.github.com>
2024-12-05 00:14:20 +0100

    Update paystack.php, removed my keys

--- config/paystack.php
+++ config/paystack.php
@@ -2,5 +2,5 @@
 return [
     'secret_key' => 'your secret key',
     'public_key' => 'your public key',
-    'base_url' => 'https://api.paystack.com/',
+    'base_url' => 'https://api.paystack.co/',
 ];
