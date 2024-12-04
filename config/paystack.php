7ee09dca1ec352e0591e1a889c2eee637e114d2d
Toptea444 <142181008+Toptea444@users.noreply.github.com>
2024-12-05 00:09:08 +0100

    Update paystack.php, removed my keys

--- config/paystack.php
+++ config/paystack.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'secret_key' => 'sk_live_0f2efa47723501c662cb7780747d44072e98a566',
-    'public_key' => 'pk_live_820417d74ac07ee40dfbfb13935082254c24f4dd',
-    'base_url' => 'https://api.paystack.co/',
+    'secret_key' => 'your secret key',
+    'public_key' => 'your public key',
+    'base_url' => 'https://api.paystack.com/',
 ];
